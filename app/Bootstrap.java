bb1c3cffce66f13aea9daa9a190afe764dd93348
Pascal Abaziou <pascal.abaziou@gmail.com>
2011-11-20 01:21:51 +0100

    change bootstrap

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -10,7 +10,7 @@ public class Bootstrap extends Job {
     public void doJob() {
         // Check if the database is empty
 //        if(User.count() == 0) {
-    		User.deleteAll();
+//    		User.deleteAll();
             Fixtures.loadModels("initial-data.yml");
 //        }
     }
