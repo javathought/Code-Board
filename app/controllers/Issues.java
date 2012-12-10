044e579b734ccd68eeb40a268ef050f22a7506c1
javathought <pascal.abaziou@gmail.com>
2012-12-10 21:36:27 +0100

    Add controls of null values for Postgres/heroku (2)

--- app/controllers/Issues.java
+++ app/controllers/Issues.java
@@ -14,6 +14,7 @@ import play.data.validation.Valid;
 import play.data.validation.Validation;
 import play.mvc.*;
 
+@With(Secure.class)
 public class Issues extends Main {
 
     
@@ -51,6 +52,7 @@ public class Issues extends Main {
 	    }
 
 	public static void myIssues() {
+		
 		List<Issue> issues = Issue.find("assignee.login = ? ", Security.connected()).fetch();
 
 		render("@issues", issues);
